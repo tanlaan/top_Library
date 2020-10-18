e12025c87c944a13147cb89ed2b393bad0fe99f9
tanlaan <tanlaan33@gmail.com>
2020-10-18 13:21:39 -0700

    Added beginning of Book constructor

--- library.js
+++ library.js
@@ -1,9 +1,16 @@
 let myLibrary = []
 
-function Book() {
+function Book(title, author, pages, read) {
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.read = read
+}
 
+Book.prototype.info = function() {
+    return `${title} by ${author}, ${pages} pages, ` + (read ? 'have read' : 'not read yet')
 }
 
 function addBookToLibrary() {
-    
+
 }
